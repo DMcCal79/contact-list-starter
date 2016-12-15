3e8e035d79bdb4b3a0d6ba168dfef207d5bb8a6b
DMcCal79 <dave_mccalmont@hotmail.com>
2016-12-14 22:46:43 -0600

    Refactored classes to functional components

---
+++ src/ContactList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Contact from './Contact';
+
+const ContactList = (props) => {
+
+    return(
+      <ul className="contact-list">
+        {props.contacts.map(contact => {
+          return (
+            <Contact
+              key={contact._id}
+              name={contact.name}
+              avatar={contact.avatar}
+              occupation={contact.occupation}
+              />
+          )
+        })}
+      </ul>
+    );
+}
+
+export default ContactList;
